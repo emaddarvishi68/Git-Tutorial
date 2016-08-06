a426fadd368ce9436dfafb35ed5f5c9172b10cd6
emaddarvishi68 <darvishi.emad@gmail.com>
2016-08-06 22:39:03 +0430

    Commit #4

---
+++ Git Tutorial/src/com/emad/Hello.java
@@ -0,0 +1,9 @@
+package com.emad;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+		System.out.println("Goodby World !!!!");
+		System.out.println("For Fetch Operation");
+	}
+}
